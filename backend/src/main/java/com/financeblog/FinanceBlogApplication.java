3f0748a509e6ef36a1bdc1df11bc0b3fc876830d
Sagar <gupta.sagar2705@gmail.com>
2026-04-06 20:51:37 +0530

    TO check direct commit is possible

--- backend/src/main/java/com/financeblog/FinanceBlogApplication.java
+++ backend/src/main/java/com/financeblog/FinanceBlogApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class FinanceBlogApplication {
-    /* Start Point of tha application */
     public static void main(String[] args) {
         SpringApplication.run(FinanceBlogApplication.class, args);
     }
